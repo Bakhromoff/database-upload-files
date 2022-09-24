f4a7c4b2e88674bd126891577f21c6db553e6bb5
Bakhromoff <hotamtoy9007@gmail.com>
2022-09-24 12:47:22 +0500

    add functions.php

--- include/functions.php
+++ include/functions.php
@@ -1,9 +1,19 @@
 <?php
-function error() {
-  if(isset($_GET['error'])) {
+function message() {
+  if(isset($_GET['er'])) {
     ?>
-    <div class="col-6 offset-3">
-    <p class="mt-4 alert alert-danger text-center"><?=$_GET['error'];?></p>
+    <div class="container">
+    <div class="col-4 offset-4">
+    <p class="mt-2 alert alert-danger text-center"><?=$_GET['er'];?></p>
+    </div>
+    </div>
+    <?php
+  } elseif(isset($_GET['s'])) {
+    ?>
+    <div class="container">
+    <div class="col-4 offset-4">
+    <p class="mt-2 alert alert-success text-center"><?=$_GET['s'];?></p>
+    </div>
     </div>
     <?php
   }
