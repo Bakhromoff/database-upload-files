6024304ed739964a5d31e21c43a891a29a7815dd
Bakhromoff <hotamtoy9007@gmail.com>
2022-09-24 12:51:22 +0500

    add index.php and create galllery items which provided with images from database

--- include/configuration.php
+++ include/configuration.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect('localhost', 'root', 'root', 'base9') or die(mysqli_error($con));
+$con = mysqli_connect('localhost', 'root', 'root', 'base9');
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,137 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
+<?php
+include "include/header.php";
+?>
 
-</body>
-</html>
+<div class="container">
+
+<!-- Modal gallery -->
+<section class="">
+  <!-- Section: Images -->
+  <section class="">
+    <div class="row">
+    <?php
+  $sql = "select * from gallery";
+  $query = mysqli_query($con,$sql);
+  while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+    ?>
+
+<div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+        <p class="text-center"><?=$row['title'];?></p>
+          <img
+            src="images/<?=$row['image'];?>"
+            class="w-100"
+          />
+          <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal1">
+            <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
+          </a>
+        </div>
+      </div>
+    <?php
+  }
+    ?>
+
+
+    </div>
+  </section>
+  <!-- Section: Images -->
+
+  <!-- Section: Modals -->
+  <section class="">
+    <!-- Modal 1 -->
+    <div
+      class="modal fade"
+      id="exampleModal1"
+      tabindex="-1"
+      aria-labelledby="exampleModal1Label"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="ratio ratio-16x9">
+            <iframe
+              src="https://www.youtube.com/embed/A3PDXmYoF5U"
+              title="YouTube video"
+              allowfullscreen
+            ></iframe>
+          </div>
+
+          <div class="text-center py-3">
+            <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Modal 2 -->
+    <div
+      class="modal fade"
+      id="exampleModal2"
+      tabindex="-1"
+      aria-labelledby="exampleModal2Label"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="ratio ratio-16x9">
+            <iframe
+              src="https://www.youtube.com/embed/wTcNtgA6gHs"
+              title="YouTube video"
+              allowfullscreen
+            ></iframe>
+          </div>
+
+          <div class="text-center py-3">
+            <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Modal 3 -->
+    <div
+      class="modal fade"
+      id="exampleModal3"
+      tabindex="-1"
+      aria-labelledby="exampleModal3Label"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="ratio ratio-16x9">
+            <iframe
+              src="https://www.youtube.com/embed/vlDzYIIOYmM"
+              title="YouTube video"
+              allowfullscreen
+            ></iframe>
+          </div>
+
+          <div class="text-center py-3">
+            <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Section: Modals -->
+</section>
+<!-- Modal gallery -->
+
+
+  <div class="row">
+    <div class="col-2 ">
+      <a class="btn btn-primary mt-4 mb-4" href="add.php">Add new</a>
+    </div>
+  </div>
+</div>
+
+<?php
+include "include/footer.php";
+?>
